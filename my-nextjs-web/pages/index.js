d58c20d3b613c8009bb28c4f405145f204123269
stalynAlejandro <saavalencia97@gmail.com>
2020-09-04 00:38:01 +0200

    react native for web & next.js

---
+++ my-nextjs-web/pages/index.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App(props){
+    return(
+        <View>
+            <Text>
+                React Native for Web & Next.js
+            </Text>
+        </View>
+    )
+}
