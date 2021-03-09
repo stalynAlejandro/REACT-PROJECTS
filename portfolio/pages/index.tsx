6b63e4867ea9a822de0a8d490fa2849b25657153
stalynAlejandro <saavalencia97@gmail.com>
2021-03-09 02:57:26 +0100

    first page :fire:

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,7 +1,56 @@
+import React, { useState } from 'react'
+import { TouchableOpacity, Text } from 'react-native-web'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+interface IHeader {
+  content: string,
+  setContent: Function
+}
+const HeaderMenu = ({ content, setContent }: IHeader) => {
+  return (
+    <main className={styles.headerContainer}>
+      <a href={'/'}><h2>SAAV</h2></a>
+      <div className={styles.headerRight}>
+        <TouchableOpacity onPress={() => setContent('about')}>
+          <h2>About</h2>
+        </TouchableOpacity>
+        <TouchableOpacity  onPress={() => setContent('projects')}>
+          <h2>Projects</h2>
+        </TouchableOpacity>
+      </div>
+    </main >
+  )
+}
+
+const HomeContent = () => {
+  return (
+    <main className={styles.contentContainer}>
+      <h1>Stalyn</h1>
+    </main>
+  )
+}
+
+const AboutContent = () => {
+  return (
+    <main className={styles.contentContainer}>
+      <h1>About</h1>
+    </main>
+  )
+}
+
+
+const ProjectsContent = () => {
+  return (
+    <main className={styles.contentContainer}>
+      <h1>Projects</h1>
+    </main>
+  )
+}
+
 
 export default function Home() {
+  const [content, setContent] = useState('home')
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,57 +58,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <HeaderMenu content={content} setContent={setContent} />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      {(content === 'home') && <HomeContent />}
+      {(content === 'about') && <AboutContent />}
+      {(content === 'projects') && <ProjectsContent />}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
