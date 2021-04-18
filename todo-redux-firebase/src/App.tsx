9a2b84db8805ad0ac31dd83055687f0d7bdf05a5
stalynAlejandro <saavalencia97@gmail.com>
2021-04-18 12:46:50 +0200

    Init :tada:

---
+++ todo-redux-firebase/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Todo - Redux - Firebase</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
