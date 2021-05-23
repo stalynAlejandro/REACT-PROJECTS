d70ab19e35c55655cc9728275357d5d9e8e36dc9
stalyn <stalyn.alcocer@idrica.com>
2021-05-23 21:39:53 +0200

    pagintation init :rocket:

--- pagination/my-pagination/src/App.tsx
+++ pagination/my-pagination/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Post } from "./Types";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState<[Post] | []>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [currrentPage, setCurrentPage] = useState<number>(1);
+  const [postPerPage, setPostPerPage] = useState<number>(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("http://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts(); // Call the function.
+  }, []); //Empty array only runs when it mounts.
+
+  console.log("POSTS", posts);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My App</h1>
     </div>
   );
 }
---
+++ pagination/my-pagination/src/components/Posts.tsx
@@ -0,0 +1,22 @@
+import { Post } from "../Types";
+
+interface IPosts {
+  posts: Post;
+  loading: number;
+}
+const Posts = ({ posts, loading }: IPosts) => {
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <ul className={"list-group mb-4"}>
+      {posts.map((post) => (
+        <li key={post.id} className={"list-group-item"}>
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default Posts;
