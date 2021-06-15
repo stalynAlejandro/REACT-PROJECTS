2470bf74320ee23c164b89de44e34b797f4ce059
stalyn <saavalencia97@gmail.com>
2021-06-15 07:33:22 +0200

    change folders directory, add Multiselect

---
+++ storybook/cra-ts/src/components/Multiselect/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MultiSelectType } from "./types";
+
+const MultiSelect = ({}: MultiSelectType) => {
+  return (
+    <div>
+      <span>Multiselect</span>
+    </div>
+  );
+};
+
+export { MultiSelect };
---
+++ storybook/cra-ts/src/stories/multiselect/Multiselect.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { MultiSelect } from "../../components/Multiselect";
+
+export default {
+  title: "MultiSelect",
+  component: MultiSelect,
+};
+
+export const MultiselectEmpty = () => {
+  return <MultiSelect />;
+};
