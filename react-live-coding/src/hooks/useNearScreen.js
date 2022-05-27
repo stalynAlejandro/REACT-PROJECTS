9f07e9faa2b08b2bb6655a4af79877b24beedaa6
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-05-27 08:49:24 +0200

    comments :head:

--- react-live-coding/src/hooks/useNearScreen.js
+++ react-live-coding/src/hooks/useNearScreen.js
@@ -2,12 +2,17 @@ import { useState, useEffect, useRef } from "react";
 
 export function useNearScreen({ distance = "100px" }) {
   const [isNearScreen, setIsNearScreen] = useState(false);
+
+  //  ref nos permite guardar valores que entre renderizados se va a manter el valor
+  //  ref cambia el valor y no vuelve a renderizar el componente
   const fromRef = useRef();
 
   useEffect(() => {
     let observer;
 
     const onChange = (entries, observer) => {
+      // Recuperamos el primer elemento que estas observando
+      // Podriamos observar mas elementos, solo vamos a observar uno
       const el = entries[0];
       if (el.isIntersecting) {
         setIsNearScreen(true);
@@ -15,6 +20,8 @@ export function useNearScreen({ distance = "100px" }) {
       }
     };
 
+    // De esta manera cargamos el polyfil 'intersection-observer' si el navegador
+    // no tiene soporte para Intersection Observer API
     Promise.resolve(
       typeof IntersectionObserver !== "undefined"
         ? IntersectionObserver
@@ -32,3 +39,8 @@ export function useNearScreen({ distance = "100px" }) {
 
   return { isNearScreen, fromRef };
 }
+
+// MDN => Intersection Observer API. Permite detectar cuando el elemento esta en el viewport
+// nos va a permitir un LazyLoad
+// Observer => Recibe dos params: callback y opciones
+// Un polyfil es una pequeña biblioteca que da una funcionalidad que le falta a tu navegador.
