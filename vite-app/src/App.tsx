949fb8cec01faa157e5c4a7126ae066d8b0db739
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-01-05 13:15:09 +0100

    workinprogress

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,27 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
-function App() {
-    const [count, setCount] = useState(0)
+const Dashboard = () => {
+    return <div>hello world</div>
+}
 
+export default function App() {
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        <div className="app">
+            <Sidebar />
+            <main className="content">
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                </Routes>
+            </main>
         </div>
     )
 }
-
-export default App
--- vite-app/src/main.tsx
+++ vite-app/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ProSidebarProvider } from 'react-pro-sidebar'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <ProSidebarProvider>
+                <App />
+            </ProSidebarProvider>
+        </BrowserRouter>
     </React.StrictMode>
 )
