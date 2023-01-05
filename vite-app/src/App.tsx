3bdfa736c83e97aa1b2beae04e4c6201221e2664
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-01-05 19:16:13 +0100

    :hammer:

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,11 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
-import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar'
-import './App.css'
+import './App.css';
 
-export default function App() {
-    return (
-        <div className="app">
-            <main className="content"></main>
-        </div>
-    )
+export function App() {
+    return <div className="App"></div>;
 }
--- vite-app/vite.config.ts
+++ vite-app/vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3030,
-    },
-    preview: {
-        port: 3031,
-    },
+  plugins: [react()],
 })
